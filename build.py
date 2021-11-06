2910c40695b57863c827bb4bd6c2d2c2576cf01f
Aapeli <git@aapelivuorinen.com>
2021-11-06 12:14:11 -0400

    Work on templates

--- build.py
+++ build.py
@@ -3,16 +3,37 @@
 import yaml
 import glob
 from markdown2 import markdown
-from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 from pathlib import Path
+import shutil
 
 build_dir = Path(".") / "build"
+shutil.rmtree(build_dir, ignore_errors=True)
 build_dir.mkdir(exist_ok=True)
 
-with open("html/template.html") as f:
-    template = Template(f.read())
+env = Environment(loader=FileSystemLoader("html"))
+template = env.get_template("template.html")
+index_template = env.get_template("index.html")
 
+## map page
+
+with open(build_dir / "index.html", "w") as o:
+    o.write(index_template.render())
+
+## place pages
 geo_data = []
+
+def rating_to_text(rating):
+    if rating == 0:
+        return "Bad"
+    elif rating == 1:
+        return "Inoffensive"
+    elif rating == 2:
+        return "Good"
+    elif rating == 3:
+        return "Phenomenal"
+    else:
+        raise ValueError("Bad rating")
 
 for place_md in glob.glob("places/*.md"):
     slug = place_md[7:-3]
@@ -23,7 +44,8 @@ for place_md in glob.glob("places/*.md"):
     html = markdown(md.strip())
     rendered = template.render(
         **meta,
-        # title=f"{meta['name']} | {meta['area']} | The Good Taste Guide",
+        taste_text=rating_to_text(meta["taste"]),
+        value_text=rating_to_text(meta["value"]),
         content=html,
     )
     out_dir = build_dir / "places" / slug
