4ae5d80cc91ca3e578fe65385bc729672cc08633
Aapeli <git@aapelivuorinen.com>
2021-11-06 13:56:58 -0400

    Clean up

--- build.py
+++ build.py
@@ -1,12 +1,13 @@
 #!/bin/python3
 
-import json
-import yaml
 import glob
-from markdown2 import markdown
-from jinja2 import FileSystemLoader, Environment
-from pathlib import Path
+import json
 import shutil
+from pathlib import Path
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+from markdown2 import markdown
 
 print("Starting build of tgtg...")
 
@@ -28,6 +29,7 @@ with open(build_dir / "error.html", "w") as o:
 ## place pages
 geojson_features = []
 
+
 def rating_to_text(rating):
     if rating == 0:
         return "Bad"
@@ -40,11 +42,14 @@ def rating_to_text(rating):
     else:
         raise ValueError("Bad rating")
 
+
 def format_title(meta):
     return f"{meta['name']} — Tasty vegan food in {meta['area']} — The Good Taste Guide"
 
+
 def format_description(meta):
     return f"Read our review on {meta['name']} at {meta['address']} in {meta['area']}, and more tasty vegan food in New York City from The Good Taste Guide!"
+
 
 for place_md in glob.glob("places/*.md"):
     slug = place_md[7:-3]
@@ -65,19 +70,15 @@ for place_md in glob.glob("places/*.md"):
     out_dir.mkdir(exist_ok=True, parents=True)
     with open(out_dir / "index.html", "w") as o:
         o.write(rendered)
-    geojson_features.append({
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [meta["lon"], meta["lat"]]
-      },
-      "properties": {**meta, "url": relative_url}
-    })
+    geojson_features.append(
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [meta["lon"], meta["lat"]]},
+            "properties": {**meta, "url": relative_url},
+        }
+    )
 
-geojson = {
-  "type": "FeatureCollection",
-  "features": geojson_features
-}
+geojson = {"type": "FeatureCollection", "features": geojson_features}
 
 with open(build_dir / "places.geojson", "w") as o:
     o.write(json.dumps(geojson))
