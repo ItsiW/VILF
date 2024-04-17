a703902b3be7e8e9c1a1c31e837739e41a0a1d07
Tristan Schrader <t0rdos@pm.me>
2024-04-17 12:09:56 -0700

    feat: prevent committing directly to develop
    it's good practice to enforce PRs

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       # TODO implement pre-commit across entire repo
       perSystem.pre-commit.settings = {
         excludes = ["static" "scripts" "raw" "places" "html"];
+        hooks.no-commit-to-branch.settings.branch = ["develop"];
         hooks.lychee.settings.flags = "--exclude googleapis";
         hooks.markdownlint.settings.configuration = {
           MD013.line_length = -1;
