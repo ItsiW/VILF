e5e199168cbf79b871230e78c14d8732262dd800
Tristan Schrader <t0rdos@pm.me>
2024-04-16 20:44:12 -0700

    refactor: extract out main infra chunks as modules

---
+++ infra/bucket.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  nix,
+  ...
+}:
+with nix; {
+  data.google_iam_policy.bucket.binding = let
+    roles."roles/storage.admin" = ["projectOwner:${config.provider.google.project}"];
+    roles."roles/storage.objectViewer" = ["allUsers"];
+    # roles."roles/storage.objectAdmin" = ["\${ google_service_account.vilfer.member }"];
+  in
+    mapAttrsToList (role: members: {inherit role members;}) roles;
+  resource = {
+    google_storage_bucket.main = {
+      depends_on = ["google_project_service.storage"];
+      name = "vilf-org";
+      location = "US";
+      uniform_bucket_level_access = true;
+      website.main_page_suffix = "index.html";
+      website.not_found_page = "error.html";
+    };
+    google_storage_bucket_iam_policy.main = {
+      bucket = "\${ google_storage_bucket.main.name }";
+      policy_data = "\${ data.google_iam_policy.bucket.policy_data }";
+    };
+    google_compute_backend_bucket.main = rec {
+      depends_on = ["google_project_service.compute"];
+      name = bucket_name;
+      bucket_name = "\${ google_storage_bucket.main.name }";
+      enable_cdn = true;
+      compression_mode = "DISABLED";
+      custom_response_headers = ["Strict-Transport-Security:max-age=31536000; includeSubDomains; preload"];
+    };
+  };
+}
