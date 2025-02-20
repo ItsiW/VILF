710b8d99a0a4922d8b9fd9a35591ea0d65c5d9f1
Tristan Schrader <t0rdos@pm.me>
2025-02-19 16:29:42 -0800

    feat(pre-commit): add shell linters like shellcheck + shfmt

--- flake.nix
+++ flake.nix
@@ -8,15 +8,18 @@
   outputs = inputs:
     inputs.canivete.lib.mkFlake {inherit inputs;} [] {
       imports = [./infra];
-      perSystem.canivete.sops.enable = true;
-      # TODO implement pre-commit across entire repo
-      perSystem.pre-commit.settings = {
-        excludes = ["static" "scripts" "raw" "places" "html"];
-        hooks.no-commit-to-branch.settings.branch = ["develop"];
-        hooks.lychee.settings.flags = "--exclude googleapis";
-        hooks.markdownlint.settings.configuration = {
-          MD013.line_length = -1;
-          MD033.allowed_elements = ["span"];
+      perSystem.canivete = {
+        sops.enable = true;
+        # TODO implement pre-commit across entire repo
+        pre-commit.languages.shell.enable = true;
+        pre-commit.settings = {
+          excludes = ["static" "scripts" "raw" "places" "html"];
+          hooks.no-commit-to-branch.settings.branch = ["develop"];
+          hooks.lychee.settings.flags = "--exclude googleapis";
+          hooks.markdownlint.settings.configuration = {
+            MD013.line_length = -1;
+            MD033.allowed_elements = ["span"];
+          };
         };
       };
     };
